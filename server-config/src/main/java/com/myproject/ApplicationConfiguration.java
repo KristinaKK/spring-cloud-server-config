f7b275fe4312bd77deea8a1585ef88d1275a805d
Kristina <kristina.kraljic@koncar-ket.hr>
2019-06-25 15:04:31 +0200

    disable web security

--- server-config/src/main/java/com/myproject/ApplicationConfiguration.java
+++ server-config/src/main/java/com/myproject/ApplicationConfiguration.java
@@ -3,13 +3,26 @@ package com.myproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ApplicationConfiguration {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationConfiguration.class, args);
 	}
+	
+//	@EnableWebSecurity
+//	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//		@Override
+//		protected void configure(HttpSecurity http) throws Exception {
+//			http.csrf().disable();
+//		}
+//	}
 
 }
